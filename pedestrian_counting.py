45891c35d6cf41029660d0a0b9fd14324578cb8e
Anna Malik <anna.malik@fis.agh.edu.pl>
2021-06-11 11:42:32 +0200

    refactor

--- pedestrian_counting.py
+++ pedestrian_counting.py
@@ -11,5 +11,4 @@ if __name__ == '__main__':
     is_color_recognition_enabled = False # set it to true for enabling the color prediction for the detected objects
     roi = 385 # roi line position
     deviation = 1 # the constant that represents the object counting area
-    custom_object_name = 'Pedestrian'
-    object_counting_api.cumulative_object_counting_x_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, roi, deviation, custom_object_name) # counting all the objects
+    object_counting_api.cumulative_object_counting_x_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, roi, deviation)  # counting all the objects
